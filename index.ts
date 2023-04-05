c82060a5baeb22657736d65623c2e6cd02f477c8
Tristan F <LeoDog896@hotmail.com>
2023-04-05 15:56:06 -0400

    feat: initial byte reads

---
+++ index.ts
@@ -0,0 +1,34 @@
+const config = `
+[general]
+bars = 30
+[output]
+method = raw
+raw_target = /dev/stdout
+bit_format = 16bit`
+
+const tempConfig = await Deno.makeTempFile({ prefix: "cava-config", suffix: ".ini" })
+
+await Deno.writeTextFile(tempConfig, config)
+
+// run cava with the temp config file
+const cava = Deno.run({
+    cmd: ["cava", "-p", tempConfig],
+    stdout: "piped",
+    stderr: "piped",
+})
+
+// read the output of cava
+const buf = new Uint8Array(60)
+while (true) {
+    const n = await cava.stdout.read(buf)
+    if (n === null) {
+        break
+    }
+    const sample = new Uint16Array(buf.buffer, 0, 30)
+    console.log(sample)
+}
+
+await cava.status()
+
+await Deno.remove(tempConfig)
+
