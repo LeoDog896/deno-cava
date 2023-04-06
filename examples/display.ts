e91173d7fdae79407d799caeb39fca27847a3be3
Tristan F <LeoDog896@hotmail.com>
2023-04-06 19:04:07 -0400

    docs(example): true visualizer

---
+++ examples/display.ts
@@ -0,0 +1,29 @@
+import { streamLines } from "../mod.ts";
+
+function percentageToBar(percent: number, height = Deno.consoleSize().rows): string {
+    const bar = "▏▎▍▌▋▊▉█";
+
+    // find percentage of height
+    const amount = height * (percent / 100);
+
+    // amount % 1 = the remainder of the division
+    const topBar = bar[Math.floor(amount % 1 * (bar.length - 1))];
+
+    return "█".repeat(Math.floor(amount)) + topBar;
+}
+
+
+for await (const line of streamLines()) {
+    const arr = [...line];
+
+    // find the max value in the array
+    const max = Math.max(...arr);
+
+    const percentage = arr.map(n => max === 0 ? 0 : Math.round(n / max * 100));
+
+    const data = percentage.map(percentageToBar);
+
+    console.clear()
+    console.log(data.join("\n"));
+    console.log("\n")
+}
